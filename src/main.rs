42cd169cb9d4f04efead03201c289061b24df45b
rimever <mot_siva_ordin@hotmail.com>
2020-04-20 21:15:31 +0900

    RustではじめるOpenGLに従って実装

--- src/main.rs
+++ src/main.rs
@@ -74,19 +74,23 @@ fn main() {
             imgui::Window::new(im_str!("Information"))
                 .size([300.0, 200.0], imgui::Condition::FirstUseEver)
                 .build(&ui, || {
-                    ui.text(im_str!("OpenGL Test App ver 1.0"));
-                    ui.separator();
-                    ui.text(im_str!("FPS: {:.1}", ui.io().framerate));
-                    let display_size = ui.io().display_size;
-                    ui.text(format!(
-                        "Display Size: ({:.1}, {:.1})",
-                        display_size[0], display_size[1]
-                    ));
+                    ui.text(im_str!("Hello World!"));
                     let mouse_pos = ui.io().mouse_pos;
                     ui.text(format!(
-                        "Mouse Position: ({:.1}, {:.1})",
+                        "Mouse Position ({:.1},{:.1})",
                         mouse_pos[0], mouse_pos[1]
                     ));
+                    imgui::ProgressBar::new(0.6)
+                        .size([200.0,20.0])
+                        .overlay_text(im_str!("Progress!"))
+                        .build(&ui);
+                    let arr = [0.6f32,0.1f32,1.0f32,0.5f32,0.92f32,0.1f32,0.2f32];
+                    ui.plot_lines(im_str!("lines"),&arr)
+                        .graph_size([200.0,40.0])
+                        .build();
+                    ui.plot_histogram(im_str!("histgram"),&arr)
+                        .graph_size([200.0,40.0])
+                        .build();
                 });
 
             imgui_sdl2_context.prepare_render(&ui, &window);
